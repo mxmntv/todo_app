dc92134dca64afa03ed47b8e39875abf1695d001
mxmntv <mxmmmntv@gmail.com>
2021-01-15 19:16:50 +0300

    add hi

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -1,11 +1,13 @@
 package handler
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
 func (h *Handler) signUp(c *gin.Context) {
-
+	fmt.Println("Hi")
 }
 
 func (h *Handler) signIn(c *gin.Context) {
